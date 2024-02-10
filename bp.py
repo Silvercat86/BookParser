271571f70c9eefb4643eabf6b8e271364a62bd6b
Victor Vorogushin <silver.cat.vic@gmail.com>
2024-02-10 23:52:39 +0300

    1.0.13

--- bp.py
+++ bp.py
@@ -2,24 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def create_collection(full_tag, link):
+    text = full_tag.text
+    list_text = full_tag.text.split(sep=" ")
+
+    lenght = len(list_text)
+    count = list_text[lenght - 2] + " " + list_text[lenght - 1]
+    name = (text[:text.find(count)])
+    return Element(link, name, count)
 
 
-
-class BookParser():
+class BookParser:
     def __init__(self):
         self.books = []
         self.series = []
         self.authors = []
 
-    def sort_long_text(full_tag):
-        text = full_tag.text
-        list_text = full_tag.text.split(sep=" ")
-        lenght = len(list_text)
-        count = list_text[lenght - 2] + " " + list_text[lenght - 1]
-        index = (text.find(count))
-        name = (text[:index])
-        return count, name
-    def find_links(self, ask):
+    def parse_site(self, ask):
         request = requests.get(f"https://flibusta.site/booksearch?ask={ask}")
         soup = BeautifulSoup(request.text, "html.parser")
 
@@ -34,43 +33,35 @@ class BookParser():
 
         match link_type:
             case 'sequence':
-                count, name = sort_long_text(full_tag)
-                print(name,count)
-                self.series.append(Collection(link, name, count))
+                self.series.append(create_collection(full_tag, link))
 
             case 'b':
                 text = full_tag.text.split(sep=" - ")
                 if len(text) != 2:
                     return
+
                 name = text[0]
                 author = text[1]
-                self.books.append(Book(link, name, author))
+                self.books.append(Element(link, name, author))
 
             case "a":
                 text = full_tag.text
                 if text == "Авторы":
                     return
-                count, name = sort_long_text(full_tag)
-                print(name,count)
-
-                self.authors.append(Collection(link, name, count))
+                self.authors.append(create_collection(full_tag, link))
 
             case _:
                 pass
 
 
-class Book:
-    def __init__(self, link, name, author):
+class Element:
+    def __init__(self, link, name, extra):
         self.link = link
         self.name = name
-        self.author = author
-
-
-class Collection:
-    def __init__(self, link, name, count):
-        self.link = link
-        self.name = name
-        self.count = count
+        self.extra = extra
 
 
 myParser = BookParser()
+myParser.parse_site(1984)
+
+
